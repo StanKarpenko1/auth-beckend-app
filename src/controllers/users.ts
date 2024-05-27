fdd2ffe68d98c93df51c2049764933d06395c554
staskarpenko1 <karpenko.stas@gmail.com>
2024-05-27 00:42:35 -0400

    latest update, patch to fix

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { getUsers } from '../db/users';
+import { deleteUserById, getUserById, getUsers } from '../db/users';
 
 export const getAllUsers = async (req: express.Request, res: express.Response) => {
     
@@ -13,5 +13,42 @@ export const getAllUsers = async (req: express.Request, res: express.Response) =
         console.log(`from Users error : ${err}`);
         return res.sendStatus(400);
     }
+}
 
+export const deleteUser = async ( req: express.Request, res: express.Response) => {
+    try {
+        const { id } = req.params;
+        const deleteUser = await deleteUserById(id);
+
+        return res.json(deleteUser)
+
+    } catch (err) {
+        console.log(err);
+        return res.sendStatus(400); 
+    }
+}
+
+export const updateUser = async (req: express.Request, res: express.Response ) => {
+    try {
+
+        const {id} = req.params
+        const {username} = req.body
+
+        if (!username){
+            return res.sendStatus(400)
+        }
+
+        const user = await getUserById(id)
+        //@ts-ignore
+        user.username = username;
+        //@ts-ignore
+        await user.save()
+
+        return res.status(200).json(user).end()
+
+
+    } catch (err) {
+        console.log(`from update User error: ${err}`)
+        res.sendStatus(400)
+    }
 }
--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -3,6 +3,28 @@ import {  merge } from 'lodash';
 
 import { getUserBySessionToken } from '../db/users'; 
 
+export const isOwner = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try {
+        const {id} = req.params;
+        //@ts-ignore
+        const currentUserId = get(req, 'identety._id') as string
+
+        if (!currentUserId){
+            return res.sendStatus(403)
+        }
+
+        if (currentUserId.toString() !== id){
+            return res.status(403)
+        }
+
+        next()
+
+    } catch (err) {
+        console.log(`from isOwner error: ${err}`)
+        return res.sendStatus(400)
+    }
+}
+
 export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {
         const sessionToken = req.cookies['STAN-AUTH'];
